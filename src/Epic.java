6c1c1c8063470c6ee2c5929fecc78bb28876dab3
Daria Laputina <laputina975@gmail.com>
2025-05-27 23:02:30 +0200

    deleted comments

--- src/Epic.java
+++ src/Epic.java
@@ -3,7 +3,6 @@ import java.util.HashMap;
 
 public class Epic extends Task {
 
-
     HashMap<Integer, Task> subtasks = new HashMap<>();
     Epic(String name, String description){
         super(name, description);
@@ -29,18 +28,8 @@ public class Epic extends Task {
     }
 
     public void removeSubTask(int id){
-        //чтобы удалять по значению,а не по индексу
         subtasks.remove(id);
 
     }
-    /*
-    1 Epic ID=1, name=Помыться,  description=Почистить зубы,  status=IN_PROGRESS subtasks=[2, 4]
-    2 SubTask ID=2, name=name2,  description=description2,  status=IN_PROGRESS epicId=1
-    4 SubTask ID=4, name=1321312,  description=12312312,  status=NEW epicId=1
-    в этом кейсе при вводе 6ой команды id =4 вылетает out of bound, если удалять без Integer.valueOf
-    я так понимаю, потому что массив интов, он пытается удалить элемент на 4 позиции, которой нет, а мне надо,
-    чтоб удалял по переданному значению, по другому не нашла как сделать
-
-    */
 
 }
